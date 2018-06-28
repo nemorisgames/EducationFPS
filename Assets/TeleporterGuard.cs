ce822388f37f0e1ccc51a926336e9652df1244b5
njaramillo-ng <njaramillo@nemorisgames.com>
2018-06-28 18:00:46 -0400

    teleporter

---
+++ Assets/TeleporterGuard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterGuard : MonoBehaviour {
+	public Teleporter teleporter;
+
+	void OnDestroy(){
+		if(teleporter == null)
+			teleporter = FindObjectOfType<Teleporter>();
+		teleporter.guards.Remove(this);
+		teleporter.CheckActive();
+	}
+}
