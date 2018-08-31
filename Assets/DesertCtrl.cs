6d785b5af2f661433151186a8a5f94b75a59ff1f
njaramillo-ng <njaramillo@nemorisgames.com>
2018-08-31 15:32:43 -0300

    escena 2: desierto

---
+++ Assets/DesertCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesertCtrl : MonoBehaviour {
+	public static DesertCtrl Instance {get{return _instance;}}
+	static DesertCtrl _instance;
+	DesertHouse [] desertHouse;
+	public int roomCount;
+
+	// Use this for initialization
+	void Awake () {
+		_instance = this;
+	}
+
+	void Start(){
+		desertHouse = FindObjectsOfType<DesertHouse>();
+		for(int i = 0; i < desertHouse.Length; i++)
+			desertHouse[i].index = i;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void EnteredRoom(int i){
+		roomCount++;
+		if(roomCount == desertHouse.Length){
+			Debug.Log("Show note");
+		}
+	}
+}
---
+++ Assets/DesertHouse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesertHouse : MonoBehaviour {
+	GameObject houseObjects;
+	public int index;
+	public bool enabledObjects = false;
+
+	// Use this for initialization
+	void Start () {
+		houseObjects = transform.Find("Objects").gameObject;
+		if(houseObjects.activeInHierarchy)
+			houseObjects.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void EnableObjects(bool b){
+		houseObjects.SetActive(b);
+		enabledObjects = b;
+	}
+
+	void OnTriggerEnter(Collider c){
+		if(c.tag == "Player" && !enabledObjects){
+			EnableObjects(true);
+			DesertCtrl.Instance.EnteredRoom(index);
+		}
+	}
+}
